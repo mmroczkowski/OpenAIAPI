967b8fe6a5342290d6b927f17a5a09faba6ac558
Marcin Mroczkowski <marcinmroczkowski@o2.pl>
2023-10-06 17:38:52 +0200

    parallel execution with retries

--- src/main/java/ai/optfor/springopenaiapi/OpenAIApi.java
+++ src/main/java/ai/optfor/springopenaiapi/OpenAIApi.java
@@ -3,13 +3,15 @@ package ai.optfor.springopenaiapi;
 import ai.optfor.springopenaiapi.cache.DefaultPromptCache;
 import ai.optfor.springopenaiapi.cache.PromptCache;
 import ai.optfor.springopenaiapi.model.*;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES;
 
@@ -20,6 +22,8 @@ public class OpenAIApi {
     private final RestTemplate restTemplate;
 
     private final PromptCache promptCache;
+
+    private final ExecutorService executorService;
 
     public OpenAIApi(String openaiKey, RestTemplate restTemplate, PromptCache promptCache) {
         this.restTemplate = (restTemplate == null ? new RestTemplate() : restTemplate);
@@ -35,8 +39,8 @@ public class OpenAIApi {
         };
 
         this.restTemplate.setInterceptors(List.of(interceptor));
-
         mapper.configure(FAIL_ON_UNKNOWN_PROPERTIES, false);
+        this.executorService = Executors.newFixedThreadPool(3);
     }
 
     public ChatCompletionResponse chat(String model, String prompt, String role, Integer maxTokens, double temperature) {
@@ -44,29 +48,38 @@ public class OpenAIApi {
     }
 
     public ChatCompletionResponse chat(String model, List<ChatMessage> chats, int maxTokens, double temperature) {
-        if (Double.compare(temperature, 0) == 0) {
+        Future<ChatCompletionResponse> future = executorService.submit(() -> chatInternal(model, chats, maxTokens, temperature));
+
+        try {
+            return future.get();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private ChatCompletionResponse chatInternal(String model, List<ChatMessage> chats, int maxTokens, double temperature) {
+        int retryCount = 0;
+        while (true) {
             try {
-                String cached = promptCache.get(createKey(model, chats, maxTokens));
-                if (cached != null) {
-                    return mapper.readValue(cached, ChatCompletionResponse.class);
+                if (Double.compare(temperature, 0) == 0) {
+                    String cached = promptCache.get(createKey(model, chats, maxTokens));
+                    if (cached != null) {
+                        return mapper.readValue(cached, ChatCompletionResponse.class);
+                    }
                 }
-            } catch (JsonProcessingException e) {
-                throw new RuntimeException(e);
+
+                ChatCompletionResponse response = restTemplate.postForObject("https://api.openai.com/v1/chat/completions",
+                        new ChatCompletionRequest(model, chats, temperature, maxTokens), ChatCompletionResponse.class);
+
+                if (Double.compare(temperature, 0) == 0) {
+                    promptCache.put(createKey(model, chats, maxTokens), mapper.writeValueAsString(response));
+                }
+
+                return response;
+            } catch (Exception e) {
+                if (++retryCount == 3) throw new RuntimeException(e);
             }
         }
-
-        ChatCompletionResponse response = restTemplate.postForObject("https://api.openai.com/v1/chat/completions",
-                new ChatCompletionRequest(model, chats, temperature, maxTokens), ChatCompletionResponse.class);
-
-        if (Double.compare(temperature, 0) == 0) {
-            try {
-                promptCache.put(createKey(model, chats, maxTokens), mapper.writeValueAsString(response));
-            } catch (JsonProcessingException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-        return response;
     }
 
     public EmbeddingData embedding(String model, String content) {
