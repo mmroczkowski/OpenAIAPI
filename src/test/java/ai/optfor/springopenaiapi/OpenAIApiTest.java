9f64d3e42c6f3179bf8a4330b650cb8f2440d8f5
Marcin Mroczkowski <marcinmroczkowski@o2.pl>
2023-10-06 15:19:27 +0200

    unused imports

--- src/test/java/ai/optfor/springopenaiapi/OpenAIApiTest.java
+++ src/test/java/ai/optfor/springopenaiapi/OpenAIApiTest.java
@@ -6,8 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.cache.Cache;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
