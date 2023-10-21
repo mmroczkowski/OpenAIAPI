e0f5e2c25a61f59693218d3cc677c56bc868b3ec
Marcin Mroczkowski <marcinmroczkowski@o2.pl>
2023-10-21 17:16:54 +0200

    add key variability per call

--- src/main/java/ai/optfor/springopenaiapi/OpenAIApi.java
+++ src/main/java/ai/optfor/springopenaiapi/OpenAIApi.java
@@ -25,37 +25,23 @@ public class OpenAIApi {
 
     private final ObjectMapper mapper = new ObjectMapper();
 
-    private final RestTemplate restTemplate;
-
     private final PromptCache promptCache;
 
     private final ExecutorService executorService;
 
-    public OpenAIApi(String openaiKey, RestTemplate restTemplate, PromptCache promptCache) {
-        this.restTemplate = (restTemplate == null ? new RestTemplate() : restTemplate);
+    public OpenAIApi(PromptCache promptCache) {
         this.promptCache = (promptCache == null ? new DefaultPromptCache() : promptCache);
-        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
-        requestFactory.setConnectTimeout(1000 * 5);
-        requestFactory.setReadTimeout(1000 * 60 * 10);
-        this.restTemplate.setRequestFactory(requestFactory);
-
-        ClientHttpRequestInterceptor interceptor = (request, body, execution) -> {
-            request.getHeaders().add("Authorization", "Bearer " + openaiKey);
-            return execution.execute(request, body);
-        };
-
-        this.restTemplate.setInterceptors(List.of(interceptor));
         mapper.configure(FAIL_ON_UNKNOWN_PROPERTIES, false);
         mapper.enable(SerializationFeature.INDENT_OUTPUT);
         this.executorService = Executors.newFixedThreadPool(3);
     }
 
-    public ChatCompletionResponse chat(String model, String prompt, String role, Integer maxTokens, double temperature) {
-        return chat(model, List.of(ChatMessage.roleMessage(role), ChatMessage.contentMessage(prompt)), maxTokens, temperature);
+    public ChatCompletionResponse chat(String model, String prompt, String role, Integer maxTokens, double temperature, String openaiKey) {
+        return chat(model, List.of(ChatMessage.roleMessage(role), ChatMessage.contentMessage(prompt)), maxTokens, temperature, openaiKey);
     }
 
-    public ChatCompletionResponse chat(String model, List<ChatMessage> chats, int maxTokens, double temperature) {
-        Future<ChatCompletionResponse> future = executorService.submit(() -> chatInternal(model, chats, maxTokens, temperature));
+    public ChatCompletionResponse chat(String model, List<ChatMessage> chats, int maxTokens, double temperature, String openaiKey) {
+        Future<ChatCompletionResponse> future = executorService.submit(() -> chatInternal(model, chats, maxTokens, temperature, openaiKey));
 
         try {
             return future.get();
@@ -64,7 +50,8 @@ public class OpenAIApi {
         }
     }
 
-    private ChatCompletionResponse chatInternal(String model, List<ChatMessage> chats, int maxTokens, double temperature) {
+    private ChatCompletionResponse chatInternal(String model, List<ChatMessage> chats, int maxTokens, double temperature, String openaiKey) {
+        RestTemplate restTemplate = prepareRestTemplate(openaiKey);
         int retryCount = 0;
         while (true) {
             try {
@@ -95,22 +82,39 @@ public class OpenAIApi {
         }
     }
 
-    public EmbeddingResponse embedding(String model, String content) {
-        return embedding(model, List.of(content));
+    public EmbeddingResponse embedding(String model, String content, String openaiKey) {
+        return embedding(model, List.of(content), openaiKey);
     }
 
-    public EmbeddingResponse embedding(String model, List<String> content) {
+    public EmbeddingResponse embedding(String model, List<String> content, String openaiKey) {
+        RestTemplate restTemplate = prepareRestTemplate(openaiKey);
         EmbeddingRequest request = new EmbeddingRequest(model, content);
         try {
             log.info("Sending request to OpenAI API: {}", mapper.writeValueAsString(request));
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
-        EmbeddingResponse response = restTemplate.postForObject("https://api.openai.com/v1/embeddings", request, EmbeddingResponse.class);
-        return response;
+        return restTemplate.postForObject("https://api.openai.com/v1/embeddings", request, EmbeddingResponse.class);
     }
 
     private String createKey(String model, List<ChatMessage> chats, int maxTokens) {
         return model + chats + maxTokens;
+    }
+
+    private RestTemplate prepareRestTemplate(String openaiKey) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(1000 * 5);
+        requestFactory.setReadTimeout(1000 * 60 * 10);
+        restTemplate.setRequestFactory(requestFactory);
+
+        ClientHttpRequestInterceptor interceptor = (request, body, execution) -> {
+            request.getHeaders().add("Authorization", "Bearer " + openaiKey);
+            return execution.execute(request, body);
+        };
+
+        restTemplate.setInterceptors(List.of(interceptor));
+        return restTemplate;
     }
 }
