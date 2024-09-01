d2b43731e10bf3a17e85f71f2c7a4ce5e63604bd
Marcin Mroczkowski <marcinmroczkowski@o2.pl>
2024-09-01 23:51:57 +0200

    Add new models

--- src/main/java/ai/optfor/springopenaiapi/enums/LLMModel.java
+++ src/main/java/ai/optfor/springopenaiapi/enums/LLMModel.java
@@ -12,7 +12,9 @@ public enum LLMModel {
     GPT_3_5_TURBO("gpt-3.5-turbo", "0.0005", "0.0015"),
     GPT_3_5_TURBO_16K("gpt-3.5-turbo-16k", "0.0015", "0.0020"),
     GPT_4_VISION_PREVIEW("gpt-4-vision-preview", "0.01", "0.03"),
-    GPT_4_O("gpt-4o", "0.005", "0.015");
+    GPT_4_O("gpt-4o", "0.005", "0.015"),
+    GPT_4_O_2024_08_06("gpt-4o-2024-08-06", "0.00250", "0.01000"),
+    GPT_4_O_MINI("gpt-4o-mini", "0.000150", "0.000600"),;
 
     private final String apiName;
     private final String promptCost;
